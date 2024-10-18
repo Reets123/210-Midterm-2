919691fcf77f37d11738ce53bc66697ace8e9eb9
Reets123 <ibrahimalatig@gmail.com>
2024-10-18 03:16:12 +0000

    commit 2

--- main.cpp
+++ main.cpp
@@ -34,48 +34,35 @@ public:
     DoublyLinkedList() : head(nullptr), tail(nullptr) {}
 
     // method to insert a new node
-    void insert_after(int value, int position) {
+    void insert_after(string value, int position) {
         if (position < 0) {
             cout << "Position must be >= 0." << endl;
-            return; // if not exit the method
+            return;
         }
-
-        Node* newNode = new Node(value); // Create a new node with the given value.
-        
-        // If the list is empty
+        Node* newNode = new Node(value);
         if (!head) {
             head = tail = newNode;
             return;
         }
-
         Node* temp = head;
-        // Traverse to the specified position
-        for (int i = 0; i < position && temp; ++i) {
+        for (int i = 0; i < position && temp; ++i)
             temp = temp->next;
-        }
-
-        // If position exceeds list size
         if (!temp) {
             cout << "Position exceeds list size. Node not inserted.\n";
             delete newNode;
             return;
         }
-
-        // Insert the new node
-        newNode->next = temp->next; // new node's next to the current node's 
-        
-        newNode->prev = temp; // new node's previous to current node.
-        
-        if (temp->next) // If current node has a next node.
+        newNode->next = temp->next;
+        newNode->prev = temp;
+        if (temp->next)
             temp->next->prev = newNode;
         else
-            tail = newNode; // Update if we're adding at the end.
-        
-        temp->next = newNode; // Link current node next to the new node
+            tail = newNode;
+        temp->next = newNode;
     }
 
     // method to delete first value in the list
-    void delete_val(int value) {
+    void delete_val(string value) {
         if (!head) return; // if list empty exit
 
         Node* temp = head;
@@ -101,46 +88,11 @@ public:
         delete temp;  // Free memory of removed node.
     }
 
-    // method to delte a node
-    void delete_pos(int pos) {
-        if (!head) {
-            cout << "List is empty." << endl;
-            return;
-        }
-
-        if (pos == 1) { // If we need to delete the first position.
-            pop_front();
-            return;
-        }
-
-        Node* temp = head; // start from head
+    void pop_front() {
         
-        for (int i = 1; i < pos; i++) { // find the node at position
-            if (!temp) {
-                cout << "Position doesn't exist." << endl;
-                return;
-            }
-            temp = temp->next;
-        }
 
-        // If position doesn't exist
-        if (!temp) {
-            cout << "Position doesn't exist." << endl;
-            return;
-        }
-        
-        // Adjust connections to remove the node
-        if (!temp->next) {
-            pop_back();
-            return;
-        }
-
-        // Update links to remove the node from the list
-        Node* tempPrev = temp->prev;
-        tempPrev->next = temp->next;
-        temp->next->prev = tempPrev;
-        delete temp;
     }
+    
 
     // Method to add a node at the end of the list.
     void push_back(int v) {
